5838e8ccfd8fdf41bc7d6cb4294f23deab849c6d
Szymon Gałecki <szymongalecki@gmail.com>
2022-05-07 21:52:53 +0200

    never ending producer-consumer solution

---
+++ pc.go
@@ -0,0 +1,50 @@
+// Producer-consumer, message passing solution
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+// buffer channel, bufferSize = 0 - blocking channel, bufferSize > 1 - buffered channel
+var bufferSize int = 0
+var buffer = make(chan int, bufferSize)
+var runforever = make(chan bool)
+
+// producers and consumers count
+const producerCount int = 2
+const consumerCount int = 5
+
+// simulating work and making output observable
+func sleep() {
+	time.Sleep(time.Duration(rand.Intn(2000)+500) * time.Millisecond)
+}
+
+func producer(id int) {
+	for {
+		sleep()
+		num := rand.Intn(100)
+		buffer <- num
+	}
+}
+
+func consumer(id int) {
+	for {
+		num := <-buffer
+		sleep()
+		fmt.Printf("Consumer%d: %d\n", id, num)
+	}
+}
+
+func main() {
+	for i := 0; i < producerCount; i++ {
+		go producer(i + 1)
+	}
+
+	for i := 0; i < consumerCount; i++ {
+		go consumer(i + 1)
+	}
+	// stop execution with keyboard interrupt (ctrl + c)
+	<-runforever
+}
