19db014a4b3782b3b11c76dbcc319e5935bea8f7
Szymon Gałecki <szymongalecki@gmail.com>
2022-09-16 13:02:16 +0200

    updated minimal solution, continous execution

--- pc.go
+++ pc.go
@@ -3,47 +3,38 @@ package main
 import (
 	"fmt"
 	"math/rand"
-	"time"
 )
 
-// producerCount >= 1, consumerCount >= 1
-const producerCount int = 2
-const consumerCount int = 5
-
-// bufferSize >= 0 (>= 1 for Dijkstra's assumption)
-var bufferSize int = 2
-var buffer = make(chan int, bufferSize)
-var runforever = make(chan bool)
-
-// sleep is for the output to be nicer, it is not a vital part of the algorithm
-func sleep() {
-	time.Sleep(time.Duration(rand.Intn(1000)) * time.Millisecond)
-}
-
-func producer(id int) {
-	for {
-		sleep()
-		num := rand.Intn(100)
-		buffer <- num
-	}
-}
-
-func consumer(id int) {
-	for {
-		sleep()
-		num := <-buffer
-		fmt.Printf("Consumer%d: %d\n", id, num)
-	}
-}
-
 func main() {
+	// producerCount >= 1, consumerCount >= 1
+	const producerCount int = 2
+	const consumerCount int = 5
+
+	// bufferSize >= 0 (>= 1 for Dijkstra's assumption)
+	var bufferSize int = 2
+	var buffer = make(chan int, bufferSize)
+	var runForever = make(chan bool)
+
+	// launch producer goroutines
 	for i := 0; i < producerCount; i++ {
-		go producer(i + 1)
+		go func(id int) {
+			for {
+				num := rand.Intn(100)
+				fmt.Printf("Producer[%d] -> %d\n", id, num)
+				buffer <- num
+			}
+		}(i + 1)
 	}
 
+	// launch consumer goroutines
 	for i := 0; i < consumerCount; i++ {
-		go consumer(i + 1)
+		go func(id int) {
+			for {
+				num := <-buffer
+				fmt.Printf("\t\t\t%2d -> Consumer[%d]\n", num, id)
+			}
+		}(i + 1)
 	}
 	// stop execution with keyboard interrupt: ctrl + c
-	<-runforever
+	<-runForever
 }
